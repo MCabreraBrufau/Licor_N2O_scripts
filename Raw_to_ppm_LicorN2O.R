03ac367742c3d9901ef01120c84d262ddd37fbb8
Miguel Cabrera Brufau <mcabrerabrufau@gmail.com>
2024-11-12 19:48:19 +0100

    Update Raw_to_ppm_LicorN2O.R
    Added tasks to perform

--- Raw_to_ppm_LicorN2O.R
+++ Raw_to_ppm_LicorN2O.R
@@ -10,6 +10,10 @@
 
 #Cosas para hacer: 
 
+#Implementar collección de linea base: "lab_baseline" y "loop_baseline" en protocolo de lab, extraer para cada dia estos datos, guardar valores medios, desviacion, nobs. 
+
+#Implementar en el protocolo dos inyecciones de estandar interno: 6ppm_1ml y lab_1ml. Para calidad, podremos utilizar 6ppm_1ml como drift, y lab_1ml vs lab_baseline como estandar interno. 2 puntos de calibracion diarios 6ppm y aire ambiental (autocalibracion)
+
 #crear un script separado para obtener injection_sequence (label, start, stop) para cada rawfile, hacerlo editable, luego importar desde este script los datos con los label corregidos/eliminados/anadidos
 
 #crear loop para cada rawfile, comprobando primero si los datos estan extraidos ya (no ejecutar para todos los rawfile, sino solo para los que no tengan ya los datos extraidos, en base a un metadata_file ) 
